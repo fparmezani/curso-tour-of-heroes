571aac0b69fa0df5e2b94b2c950b645fa48f57d1
Fernando <fparmezani@hotmail.com>
2023-06-17 14:33:54 -0300

    Create Heroes Component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component , OnInit} from '@angular/core';
+import { Hero } from './hero.model';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  hero : Hero = {
+    name: 'Batman',
+    id: 20
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+
+}
